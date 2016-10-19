fixed bug in LZ 2D and implemented normalization

pci.py
@@ -13,17 +13,24 @@ import numpy as np
 DEBUG = False
 #DEBUG = True
 
-#TODO implement normalization!
-def pci(L):
-    return lz_complexity_2D(L)
+def pci(D):
 
-def lz_complexity_2D(L):
+    L = D.shape[0] * D.shape[1]
+    p1 = sum(1.0 * (D.flatten() == 1)) / L
+    p0 = 1 - p1
+    H = -p1 * np.log2(p1) -p0 * np.log2(p0)
 
-    if len(L.shape) != 2:
+    S = (L * H) / np.log2(L)
+
+    return lz_complexity_2D(D) / S
+
+def lz_complexity_2D(D):
+
+    if len(D.shape) != 2:
         raise Exception('data has to be 2D!')
 
-    l1 = L.shape[0]
-    l2 = L.shape[1]
+    L1 = D.shape[0]
+    L2 = D.shape[1]
 
     c=1
     r=1
@@ -39,7 +46,7 @@ def lz_complexity_2D(L):
         r += 1
 
         # end line + end of column? end of the algorithm...
-        if r > l2:
+        if r > L2:
             c += 1
             stop = True
         else:
@@ -56,21 +63,21 @@ def lz_complexity_2D(L):
         if q == r:
             a = i+k-1
         else:
-            a=l1
+            a=L1
 
         if DEBUG:
             n_iterations += 1
             print "Iteration #%d: (c=%d, r=%d, q=%d ,k=%d ,i=%d ,a=%d)" % (n_iterations, c, r, q, k, i, a)
 
-        d = L[i:i+k+1,r-1]
-        e = L[0:a+1,q-1]
+        d = D[i:i+k,r-1]
+        e = D[0:a,q-1]
 
         found = np.all(rolling_window(e, len(d)) == d, axis=1)
 
         if found.any():
 
             k += 1
-            if i+k > l1:
+            if i+k > L1:
 
                 (r, c, i, q, k, stop) = end_of_line(r, c, i, q, k, stop)
 
@@ -81,7 +88,7 @@ def lz_complexity_2D(L):
 
                 c += 1
                 i = i + k
-                if i + 1 > l1:
+                if i + 1 > L1:
 
                     (r, c, i, q, k, stop) = end_of_line(r, c, i, q, k, stop)
 

test.py
@@ -2,33 +2,37 @@
 import pci
 import pickle
 import time
+# import numpy as np
 
 # BEWARE: this takes ~17 min in a Intel Xeon CPU E5-2630 v3 @ 2.40GHz
 
 print("Start: " + time.strftime("%c"))
 
 # test 1D algorithm
-
 lz = pci.lz_complexity('1001111011000010')
 print '1D found: %d, should be %d' % (lz, 6)
-#assert lz == 6
+assert lz == 6
 
 # test 2D algorithm (beware: this might take a few minutes!)
-
 with open('./lz_2D_example.pickle', 'r') as f:
-    known_lz = pickle.load(f)
+    known_pci = pickle.load(f)
 
-# lz_columnwise = pci.lz_complexity_2D(known_lz['data'])
-# print '2D columnwise found: %d, should be %d' % (lz_columnwise, known_lz['lz_columnwise'])
-# #assert lz_columnwise == known_lz['lz_columnwise']
+#lz_columnwise = pci.lz_complexity_2D(known_pci['data'])
+#print '2D LZ columnwise found: %d, should be %d' % (lz_columnwise, known_pci['lz_columnwise'])
+#assert lz_columnwise == known_pci['lz_columnwise']
+
+PCI_columnwise = pci.pci(known_pci['data'])
+print 'PCI columnwise found: %f, should be %f' % (PCI_columnwise, known_pci['pci_columnwise'])
+#assert np.int(100000*PCI_columnwise) == np.int(100000*known_pci['pci_columnwise']))
 
 
-#lz_linewise = pci.lz_complexity_2D(known_lz['data'].T)
-x = known_lz['data'].T
-#x = x[:100,:100]
-lz_linewise = pci.lz_complexity_2D(x)
-print '2D linewise found: %d, should be %d' % (lz_linewise, known_lz['lz_linewise'])
-#assert lz_linewise == known_lz['lz_linewise']
+#lz_linewise = pci.lz_complexity_2D(known_pci['data'].T)
+#print '2D linewise found: %d, should be %d' % (lz_linewise, known_pci['lz_linewise'])
+#assert lz_linewise == known_pci['lz_linewise']
+
+#x = np.array([[0,0,1],[1,0,1],[1,0,1]])
+#lz_2D = pci.lz_complexity_2D(x)
+#print '2D linewise found: %d, should be %d' % (lz_2D, 5)
 
 print("End: " + time.strftime("%c"))
 