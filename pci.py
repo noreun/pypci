5726d607e7112b6e8fc110d5f2ec83ce4d5f1fbe
Leonardo Barbosa <lsbarbosa@gmail.com>
2016-10-20 14:31:56 -0500

    implemented search with bitarray (3x faster)

--- pci.py
+++ pci.py
@@ -5,19 +5,21 @@
 #
 # Leonardo Barbosa
 # leonardo.barbosa@usp.br
-# 21/09/2016
 #
+# 21/09/2016 : First version with rolling window
+# 19/10/2016 : Fixed bug due to indexing problem
+#              Implemented normalization
+# 20/10/2016 : Implemented search with bitarray (3x faster)
 
 import numpy as np
-
-DEBUG = False
-#DEBUG = True
+from bitarray import bitarray
 
 def calculate(D):
 
     return lz_complexity_2D(D) / pci_norm_factor(D)
 
 def pci_norm_factor(D):
+
     L = D.shape[0] * D.shape[1]
     p1 = sum(1.0 * (D.flatten() == 1)) / L
     p0 = 1 - p1
@@ -32,35 +34,29 @@ def lz_complexity_2D(D):
     if len(D.shape) != 2:
         raise Exception('data has to be 2D!')
 
-    L1 = D.shape[0]
-    L2 = D.shape[1]
-
-    c=1
-    r=1
-    q=1
-    k=1
-    i=1
-
+    # initialize
+    (L1, L2) = D.shape
+    c=1; r=1; q=1; k=1; i=1
     stop = False
 
-    def end_of_line(r, c, i, q, k, stop):
+    # convert each column to a sequence of bits
+    bits = [None] * L2
+    for y in range(0,L2):
+        bits[y] = bitarray(D[:,y].tolist())
 
-        # go to the next column
+    # action to perform every time it reaches the end of the column
+    def end_of_column(r, c, i, q, k, stop):
         r += 1
-
-        # end line + end of column? end of the algorithm...
         if r > L2:
             c += 1
             stop = True
         else:
-            # reset for next line
             i = 0
             q = r - 1
             k = 1
-
         return r, c, i, q, k, stop
 
-    n_iterations = 0
+    # main loop
     while not stop:
 
         if q == r:
@@ -68,88 +64,32 @@ def lz_complexity_2D(D):
         else:
             a=L1
 
-        if DEBUG:
-            n_iterations += 1
-            print "Iteration #%d: (c=%d, r=%d, q=%d ,k=%d ,i=%d ,a=%d)" % (n_iterations, c, r, q, k, i, a)
+        # binary search
+        #d = bits[r-1][i:i+k]
+        #e = bits[q-1][0:a]
+        #found = not not e.search(d, 1)
+        found = not not bits[q-1][0:a].search(bits[r-1][i:i+k], 1)
 
-        d = D[i:i+k,r-1]
-        e = D[0:a,q-1]
+        ## rowlling window (3x slower)
+        #d = D[i:i+k,r-1]
+        #e = D[0:a,q-1]
+        #found = np.all(_rolling_window(e, len(d)) == d, axis=1).any()
 
-        found = np.all(_rolling_window(e, len(d)) == d, axis=1)
-
-        if found.any():
-
+        if found:
             k += 1
             if i+k > L1:
-
-                (r, c, i, q, k, stop) = end_of_line(r, c, i, q, k, stop)
-
+                (r, c, i, q, k, stop) = end_of_column(r, c, i, q, k, stop)
         else:
-
             q -= 1
             if q < 1:
-
                 c += 1
                 i = i + k
                 if i + 1 > L1:
-
-                    (r, c, i, q, k, stop) = end_of_line(r, c, i, q, k, stop)
-
+                    (r, c, i, q, k, stop) = end_of_column(r, c, i, q, k, stop)
                 else:
                     q = r
                     k = 1
 
     return c
 
-def _rolling_window(a, size):
-    shape = a.shape[:-1] + (a.shape[-1] - size + 1, size)
-    strides = a.strides + (a. strides[-1],)
-    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
-
-# 1D Lempel-Ziv implementation from: http://stackoverflow.com/questions/4946695/calculating-lempel-ziv-lz-complexity-aka-sequence-complexity-of-a-binary-str
-# Kaspar, F. Schuster, H. Easily calculable measure for the complexity of spatiotemporal patterns. Physical Review A, vol 36, n. 2, p 842.
-def lz_complexity(s):
-    i, k, l = 0, 1, 1
-    k_max = 1
-    n = len(s) - 1
-    c = 1
-    while True:
-        if s[i + k - 1] == s[l + k - 1]:
-            k += 1
-            if l + k >= n - 1:
-                c += 1
-                break
-        else:
-            if k > k_max:
-               k_max = k
-            i += 1
-            if i == l:
-                c += 1
-                l = l + k_max
-                if l + 1 > n:
-                    break
-                else:
-                    i = 0
-                    k = 1
-                    k_max = 1
-            else:
-                k = 1
-    return c
-
-
-#data = (1 * (np.random.rand(100,100) > .5)).astype('str')
-#data = 1 * (np.random.rand(100,100) > .5)
-#
-#data_c = pci(data)
-#print "Complexity of data : %d" % data_c
-#
-## Compare to the 1D implementation
-#
-#data_1d = data.flatten()
-#
-# data_1d_c = pci(data_1d)
-# print "Complexity of 1D data : %d" % data_1d_c
-#
-# data_1d_c2 = lz_complexity(data_1d)
-# print "Complexity 2 of 1D data : %d" % data_1d_c2
 
--- test.py
+++ test.py
@@ -4,34 +4,40 @@ import pickle
 import time
 import numpy as np
 
-# BEWARE: this takes ~17 min in a Intel Xeon CPU E5-2630 v3 @ 2.40GHz
+# BEWARE: this takes ~3 min in a Intel Xeon CPU E5-2630 v3 @ 2.40GHz
 
 print("Start: " + time.strftime("%c"))
 
-# test 1D algorithm
-lz = pci.lz_complexity('1001111011000010')
-print '1D found: %d, should be %d' % (lz, 6)
-assert lz == 6
-
-# test 2D algorithm
 with open('./lz_2D_example.pickle', 'r') as f:
     known_pci = pickle.load(f)
 
+# columnwise...
+t = time.time()
 lz_columnwise = pci.lz_complexity_2D(known_pci['data'])
-print '2D LZ columnwise found: %d, should be %d' % (lz_columnwise, known_pci['lz_columnwise'])
+elapsed = time.time() - t
+print '2D LZ columnwise found: %d, should be %d (Elapsed time : %.2f s)' % (lz_columnwise, known_pci['lz_columnwise'], elapsed)
 assert lz_columnwise == known_pci['lz_columnwise']
+
+# rolling window : 2D LZ columnwise found: 16842, should be 16842 (Elapsed time : 437.64 s)  426.53 s
+# bits sequence : 2D LZ columnwise found: 16842, should be 16842 (Elapsed time : 125.72 s)
 
 PCI_columnwise = lz_columnwise / pci.pci_norm_factor(known_pci['data'])
 print 'PCI columnwise found: %f, should be %f' % (PCI_columnwise, known_pci['pci_columnwise'])
 assert np.int(100000*PCI_columnwise) == np.int(100000*known_pci['pci_columnwise'])
 
-#lz_linewise = pci.lz_complexity_2D(known_pci['data'].T)
-#print '2D linewise found: %d, should be %d' % (lz_linewise, known_pci['lz_linewise'])
-#assert lz_linewise == known_pci['lz_linewise']
+# linewise...
+t = time.time()
+lz_linewise = pci.lz_complexity_2D(known_pci['data'].T)
+elapsed = time.time() - t
+print '2D LZ linewise found: %d, should be %d (Elapsed time : %.2f s)' % (lz_linewise, known_pci['lz_linewise'], elapsed)
+assert lz_linewise == known_pci['lz_linewise']
 
-#x = np.array([[0,0,1],[1,0,1],[1,0,1]])
-#lz_2D = pci.lz_complexity_2D(x)
-#print '2D linewise found: %d, should be %d' % (lz_2D, 5)
+# rolling window :
+# bits sequence : 2D LZ linewise found: 7518, should be 7518 (Elapsed time : 75.44 s)
+
+PCI_linewise = lz_linewise / pci.pci_norm_factor(known_pci['data'].T)
+print 'PCI linewise found: %f, should be %f' % (PCI_linewise, known_pci['pci_linewise'])
+assert np.int(100000*PCI_linewise) == np.int(100000*known_pci['pci_linewise'])
 
 print("End: " + time.strftime("%c"))
 
